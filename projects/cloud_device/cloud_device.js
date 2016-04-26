8587440f8031dbe56b69da0d54d63bfeccccdf5c
pengkobe <370251370@qq.com>
2016-04-27 00:48:22 +0800

    fix cloud_device bug

--- projects/cloud_device/cloud_device.js
+++ projects/cloud_device/cloud_device.js
@@ -64,10 +64,10 @@ $(function() {
             success: function(data, textStatus) {
                 var data = data.data;
                 for (var i = 0; i < data.length; i++) {
-                    $("#e_" + (i+1) + "_SumCount").html(data[i].SumCount);
-                    $("#e_" + (i+1) + "_engergy").html(data[i].fqfh +"&nbsp;&nbsp;"+data[i].ndl);
-                    $("#e_" + (i+1) + "_hzl").html(data[i].hzl);
-                    $("#e_" + (i+1) + "_gzl").html(data[i].gzl);
+                    $("#e_" + (i+1) + "_SumCount").html(data[i].SumCount+'<span class="unit">个</span>');
+                    $("#e_" + (i+1) + "_engergy").html(data[i].fqfh +'<span class="unit">kwh</span>'+"&nbsp;&nbsp;"+data[i].ndl+'<span class="unit">kwh/年</span>');
+                    $("#e_" + (i+1) + "_hzl").html(data[i].hzl+'<span class="unit">&nbsp;负载率</span>');
+                    $("#e_" + (i+1) + "_gzl").html((data[i].gzl*100).toFixed(0)+'%<span class="unit">&nbsp;故障率</span>');
                 }
             },
             error: function() {}
@@ -82,10 +82,10 @@ $(function() {
             success: function(data, textStatus) {
                 var data = data.data;
                 for (var i = 0; i < data.length; i++) {
-                    $("#w_" + (i+1) + "_SumCount").html(data[i].SumCount);
-                    $("#w_" + (i+1) + "_engergy").html(data[i].fqfh +"&nbsp;&nbsp;"+data[i].ndl);
-                    $("#w_" + (i+1) + "_hzl").html(data[i].hzl);
-                    $("#w" + (i+1) + "_gzl").html(data[i].gzl);
+                    $("#w_" + (i+1) + "_SumCount").html(data[i].SumCount+'<span class="unit">个</span>');
+                    $("#w_" + (i+1) + "_engergy").html(data[i].fqfh +'<span class="unit">kwh</span>'+"&nbsp;&nbsp;"+data[i].ndl+'<span class="unit">kwh/年</span>');
+                    $("#w_" + (i+1) + "_hzl").html(data[i].hzl+'<span class="unit">&nbsp;负载率</span>');
+                    $("#w_" + (i+1) + "_gzl").html((data[i].gzl*100).toFixed(0)+'%<span class="unit">&nbsp;故障率</span>');
                 }
             },
             error: function() {}
@@ -98,13 +98,13 @@ $(function() {
             type: 'POST',
             data: { Industry: 1 },
             success: function(data, textStatus) {
-              debugger;
-               var data = data.data[0];
-                $("#Data1").html(data.Data1);
-                $("#Data2").html(data.Data2);
-                $("#Area1").html(data.Area1);
-                $("#Area2").html(data.Area2);
-                $("#Energy").html(data.Energy);
+                var data = data.data[0];
+                $("#b_mj").html(data.mj+'<span class="unit">&nbsp;公顷</span>');
+                $("#b_dss_dlj").html(data.dss+'<span class="unit">&nbsp;kwh</span>'+'&nbsp;'+data.dlj+'<span class="unit">&nbsp;kwh/年</span>');
+                $("#b_sss_slj").html(data.sss+'<span class="unit">&nbsp;m</span>'+'&nbsp;'+data.slj+'<span class="unit">&nbsp;m/年</span>');
+                $("#b_yss_ylj").html(data.yss+'<span class="unit">&nbsp;l</span>'+'&nbsp;'+data.ylj+'<span class="unit">&nbsp;l/年</span>');
+                $("#b_sbsl").html(data.sbsl+'<span class="unit">&nbsp;个</span>');
+                $("#b_sjsl").html(data.sjsl+'<span class="unit">&nbsp;T</span>');
 
             },
             error: function() {
@@ -118,7 +118,15 @@ $(function() {
             dataType: "json",
             type: 'POST',
             data: { Industry: 2 },
-            success: function(data, textStatus) {},
+            success: function(data, textStatus) {
+                var data = data.data[0];
+                $("#cb_mj").html(data.mj+'<span class="unit">&nbsp;公顷</span>');
+                $("#cb_dss_dlj").html(data.dss+'<span class="unit">&nbsp;kwh</span>'+'&nbsp;'+data.dlj+'<span class="unit">&nbsp;kwh/年</span>');
+                $("#cb_sss_slj").html(data.sss+'<span class="unit">&nbsp;m</span>'+'&nbsp;'+data.slj+'<span class="unit">&nbsp;m/年</span>');
+                $("#cb_yss_ylj").html(data.yss+'<span class="unit">&nbsp;l</span>'+'&nbsp;'+data.ylj+'<span class="unit">&nbsp;l/年</span>');
+                $("#cb_sbsl").html(data.sbsl+'<span class="unit">&nbsp;个</span>');
+                $("#cb_sjsl").html(data.sjsl+'<span class="unit">&nbsp;T</span>');
+            },
             error: function() {
 
             }
