cab889fb12e7345df981685324a38dcb49d0f814
pengkobe <yipeng.info@gmail.com>
2017-02-16 10:40:58 +0800

    add session verify for countdown

--- server/routes/countdowm.js
+++ server/routes/countdowm.js
@@ -23,6 +23,7 @@ app.post('/all', function (req, res) {
   });
 });
 
+app.post('/add', checkLogin);
 app.post('/add', function (req, res) {
   var cobj = req.body;
   var countdown = new Countdown({
@@ -45,7 +46,7 @@ app.post('/add', function (req, res) {
   });
 });
 
-
+app.get('/:_id/delete', checkLogin);
 app.get('/:_id/delete', function (req, res) {
     console.log('rm' + req.params._id);
     Countdown.remove(req.params._id, function (err) {
@@ -61,4 +62,25 @@ app.get('/:_id/delete', function (req, res) {
 app.get('/edit', function (req, res) {
   res.json({ success: true });
 });
+
+
+function checkLogin(req, res, next) {
+  if (!req.session.user) {
+    req.flash('error', '未登录!');
+    res.json({success:false});
+   // res.redirect('/login');
+    return;
+  }
+  next();
+}
+
+function checkNotLogin(req, res, next) {
+  if (req.session.user) {
+    req.flash('error', '已登录!');
+    res.json({success:false});
+   // res.redirect('back');//返回之前的页面
+    return;
+  }
+  next();
+}
 module.exports = app;
