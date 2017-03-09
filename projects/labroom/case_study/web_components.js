21ca5d90aac732165a825080392e8db0c1d6c037
pengkobe <yipeng.info@gmail.com>
2017-03-09 15:40:32 +0800

    rm labroom/

--- projects/labroom/case_study/web_components.js
+++
@@ -1,9 +0,0 @@
-;
-define(function(require, exports, module) {
-    var addBox = document.querySelector('add-box');
-    var todoList = document.querySelector('todo-list');
-
-    addBox.addEventListener('add-item', function(e) {
-        todoList.addItem(e.detail);
-    });
-});
--- projects/labroom/snippets/js_position_element.js
+++
@@ -1,7 +0,0 @@
-define(function(require, exports, module) {
-    module.exports= function(){
-        $('#snippets code').each(function(i, block) {
-            hljs.highlightBlock(block);
-        });
-    }
-});
